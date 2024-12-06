72318d8f1229f1cf9420d02be1246de155a5189f
Danil <danilfs009@yandex.ru>
2024-12-06 17:54:05 +0300

    start of App

--- main.go
+++ main.go
@@ -1,1 +1,29 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	var taskOne string = "Watch go crush course"
+	watchCourse := "Watch course"
+	var rewardDesert string = "Eat cheesecake"
+
+	fmt.Println("List of Todos")
+	fmt.Println(taskOne)
+	fmt.Println(watchCourse)
+	fmt.Println("Watch video")
+	fmt.Println(rewardDesert)
+
+	fmt.Println()
+	fmt.Println("Tutorials")
+	fmt.Println(taskOne)
+
+	fmt.Println()
+	fmt.Println("Reward")
+	fmt.Println(rewardDesert)
+
+	fmt.Println()
+	fmt.Println("My Project")
+	fmt.Println(taskOne)
+
+}
